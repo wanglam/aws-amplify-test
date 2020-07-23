a85c1cf7a055f9c87684e0ede63d8d6d3e85bced
王麟 <viowan@gmail.com>
2020-07-23 19:00:57 +0800

    Change new new text

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Change new new text</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
